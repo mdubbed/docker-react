56a07800b5e377fa5ea2781b7aa2fa5f7dfe8b58
mdubbed <mdub4887@protonmali.com>
2023-05-04 20:42:33 -0400

    added travis file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Click Here ToLearn React, Bitch!
+          Click Here To Learn React, Homes    !
         </a>
       </header>
     </div>
